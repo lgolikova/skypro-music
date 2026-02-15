4f0da2161456574ba36c98a0dd5d6a432281e26a
lgolikova <milagoul@gmail.com>
2026-02-15 13:22:30 +0300

    made layout for login and register pages

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import styles from "./layout.module.css";
+
+interface AuthLayoutProps {
+    children: ReactNode;
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.containerEnter}>
+                    <div className={styles.modal__block}>
+                        <form className={styles.modal__form}>{children}</form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
